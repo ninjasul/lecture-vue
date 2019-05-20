129c30886d7808f51c8e6aecec0e0e83349f53ac
ninjasul@gmail.com <ninjasul@gmail.com>
2019-05-20 19:43:32 +0900

    handled enterKey for searchForm

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -10,6 +10,7 @@ FormView.setup = function (el) {
     this.resetEl = el.querySelector('[type=reset]');
     this.showResetBtn(false);
     this.bindEvents();
+    return this;    // builder 패턴 처럼 chaining 방식으로 설정하려면 return this; 를 해 주어야 함.
 }
 
 FormView.showResetBtn = function(show = true) {
@@ -21,8 +22,15 @@ FormView.bindEvents = function() {
     this.resetEl.addEventListener('click', e => this.onReset(e));
 }
 
-FormView.onKeyup = function () {
+FormView.onKeyup = function (e) {
     this.showResetBtn(this.inputEl.value.length);
+
+    // enter키가 입력되면 mainController에 알려줌.
+    const enterKey = 13;
+    console.log('keyCode: ', e.keyCode);
+    if (e.keyCode === enterKey) {
+        this.emit('@submit', {input: this.inputEl.value});
+    }
 }
 
 FormView.onReset = function () {
