eb97f3c6af78f40825a5127b274a54e259bb08b2
ninjasul@gmail.com <ninjasul@gmail.com>
2019-05-21 18:11:27 +0900

    implemented search result list

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,4 +1,6 @@
 import FormView from '../views/FormView.js';
+import ResultView from '../views/ResultView.js';
+import SearchModel from '../models/SearchModel.js';
 
 const tag = '[MainController]';
 
@@ -8,13 +10,27 @@ export default {
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e => this.onResetForm())
         ;
+
+        ResultView.setup(document.querySelector('#search-result'));
     },
 
     onSubmit(input) {
         console.log(tag, 'onSubmit()', input);
+        this.search(input);
     },
 
     onResetForm() {
         console.log(tag, 'onResetForm()');
+    },
+
+    search(query) {
+        console.log(tag, 'search()', query);
+        SearchModel.list(query).then(data => {
+            this.onSearchResult(data);
+        });
+    },
+
+    onSearchResult(data) {
+        ResultView.render(data);
     },
 }
