7f6221ddd774374ac16fddcfa24e1facf276b851
ninjasul@gmail.com <ninjasul@gmail.com>
2019-05-27 18:42:12 +0900

    검색 폼 구현 - 검색 결과 구현

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,11 +1,35 @@
+import SearchModel from "./models/SearchModel.js";
+
 new Vue({
     el: '#app',
     data: {
-        query: ''
+        query: '',
+        submitted: false,
+        searchResult: []
     },
     methods: {
         onSubmit(e) {
-            debugger
-        }
+            this.search();
+        },
+
+        onKeyup(e) {
+            if(!this.query.length) {
+                this.onReset();
+            }
+        },
+
+        search() {
+            SearchModel.list(this.query).then(data => {
+                this.searchResult = data;
+                this.submitted = true;
+            });
+        },
+
+        onReset(e) {
+            this.query = '';
+            this.submitted = false;
+            this.searchResult = [];
+        },
+
     }
 })
